93a353c7c36fedf2404f545e75bf04560570d204
lawalgodwin <godwinl200@gmail.com>
2024-12-30 00:50:19 +0100

    Set up Listing, Booking and Review models

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -32,7 +32,7 @@ class Booking(models.Model):
     booking_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     property = models.ForeignKey(to=Listing, related_name="bookings", related_query_name="booking", on_delete=models.CASCADE)
     guest = models.ForeignKey(to=User, related_name="bookings", related_query_name="booking", on_delete=models.CASCADE)
-    start_date = models.DateField(default=timezone.now)
+    start_date = models.DateField()
     end_date = models.DateField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=9, choices=Status, default=Status.PENDING)
