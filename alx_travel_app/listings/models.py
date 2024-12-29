5d58909c4586a1588f0a4f3470e376994ac5839e
lawalgodwin <godwinl200@gmail.com>
2024-12-30 00:52:39 +0100

    Set up Listing, Booking and Review models

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 import uuid
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
